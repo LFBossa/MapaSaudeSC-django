bf537e9f31ec86b17d4b1a40717343b21d5ec35b
Bossa <l.f.bossa@ufsc.br>
2022-05-26 16:46:33 -0300

    endpoint lista das cidades

--- mapasaude/mapa/urls.py
+++ mapasaude/mapa/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('api/estabelecimentos/cidade/<int:pk>', views.EstabelecimentoCidadeAPI, name="estabelecimentocidade"),
     path('api/estabelecimentos/tipo/<int:tipo>', views.EstabelecimentoTipoAPI, name="estabelecimentotipo"),
     path('api/estabelecimentos/tipo/', views.TiposEstabelecimentosAPI, name="tipos"),
+    path('api/cidades/nomes/', views.ListaCidadesAPI, name='listacidadesapi')
     #path('regioes')
 ]
