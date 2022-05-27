c573c6d6bd05edb9b38c6be37217835bad72f280
Bossa <l.f.bossa@ufsc.br>
2022-05-27 11:17:29 -0300

    puxando variáveis com dotenv

--- mapasaude/mapasaude/settings.py
+++ mapasaude/mapasaude/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import dotenv_values
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+config = dotenv_values()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q*nw2g-vtf!=96-4wfpyv^$+#u8=54d3ecjuy*!0-6zdky9b6a'
+SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
