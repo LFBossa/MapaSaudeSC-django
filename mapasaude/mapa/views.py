62f51d2025fa63b30a94666b620ef06204b90ccf
Bossa <l.f.bossa@ufsc.br>
2022-05-30 16:00:08 -0300

    autoindent

--- mapasaude/mapa/views.py
+++ mapasaude/mapa/views.py
@@ -14,6 +14,7 @@ from .models import Estabelecimento, Municipio, TipoEstabelecimento
 class ListaCidade(generic.ListView):
     model = Municipio
 
+
 class DetalheCidade(generic.DetailView):
     model = Municipio
     template_name = "mapas/municipio_detail.html"
@@ -21,46 +22,54 @@ class DetalheCidade(generic.DetailView):
 
 
 def indexcidade(request):
-    context = {'lista_municipios' : Municipio.objects.order_by('nome'), 'letras': [chr(x) for x in range(65,65+26)]}
+    context = {'lista_municipios': Municipio.objects.order_by(
+        'nome'), 'letras': [chr(x) for x in range(65, 65+26)]}
     return render(request, 'mapas/listacidades.html', context)
 
-def cidadedetalhe(request, pk):
-    cidade = Municipio.objects.get(pk=pk) 
 
-    context = {'cidade': cidade,  }
+def cidadedetalhe(request, pk):
+    cidade = Municipio.objects.get(pk=pk)
+
+    context = {'cidade': cidade, }
     return render(request, 'mapas/municipio_detail.html', context)
 
-def estado(request):
-    todosmunicipios = Municipio.objects.all() 
-    return render(request, 'mapas/estado.html', {'lista_municipios': todosmunicipios} )
 
+def estado(request):
+    todosmunicipios = Municipio.objects.all()
+    return render(request, 'mapas/estado.html', {'lista_municipios': todosmunicipios})
+ 
 def informacoes_estabelecimento(estabelecimento):
-    return  {'coordenadas': estabelecimento.llcoord, 
-                    'nome': estabelecimento.nome, 
-                    'endereco': estabelecimento.endereco, 
-                    'cnes': estabelecimento.cnes, 
-                    'tipo': estabelecimento.tipo.nome}
+    return {'coordenadas': estabelecimento.llcoord,
+            'nome': estabelecimento.nome,
+            'endereco': estabelecimento.endereco,
+            'cnes': estabelecimento.cnes,
+            'tipo': estabelecimento.tipo.nome}
+
 
 @cache_page(24 * 60 * 60)
-def EstabelecimentoCidadeAPI(request,pk):
+def EstabelecimentoCidadeAPI(request, pk):
     #cidade = Municipio.objects.get(pk=pk)
     estabeles = Estabelecimento.objects.filter(municipio=pk)
-    resposta = [informacoes_estabelecimento(x) for x in estabeles ]
+    resposta = [informacoes_estabelecimento(x) for x in estabeles]
     return JsonResponse(resposta, safe=False)
 
+
 @cache_page(24 * 60 * 60)
-def EstabelecimentoTipoAPI(request,tipo):
+def EstabelecimentoTipoAPI(request, tipo):
     #cidade = Municipio.objects.get(pk=pk)
     estabeles = Estabelecimento.objects.filter(tipo=tipo)
-    resposta = [informacoes_estabelecimento(x) for x in estabeles ]
+    resposta = [informacoes_estabelecimento(x) for x in estabeles]
     return JsonResponse(resposta, safe=False)
+
 
 @cache_page(24 * 60 * 60)
 def TiposEstabelecimentosAPI(request):
-    tipos = [ (x.n, x.nome) for x in TipoEstabelecimento.objects.all() ]
+    tipos = [(x.n, x.nome) for x in TipoEstabelecimento.objects.all()]
     return JsonResponse(tipos, safe=False)
 
-@cache_page(24 * 60 *60)
+
+@cache_page(24 * 60 * 60)
 def ListaCidadesAPI(request):
-    lista_cidades = [ {"ibge": x.ibge, "nome": x.nome, "regiao": x.regiao.nome } for x in  Municipio.objects.all() ]
+    lista_cidades = [{"ibge": x.ibge, "nome": x.nome,
+                      "regiao": x.regiao.nome} for x in Municipio.objects.all()]
     return JsonResponse(lista_cidades, safe=False)
